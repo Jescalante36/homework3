def9c79b958b59db218ae353e76e45d81a13238b
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-09-28 13:49:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,10 +16,6 @@
       <th>number</th>
       <th>description</th>
     </tr>
-     <tr>
-      <th>CourseID</th>
-      <th>prefix</th>
-    </tr>
   </thead>
   <tbody>
     <?php
@@ -59,4 +55,8 @@ $conn->close();
     </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
+       <tr>
+      <th>CourseID</th>
+      <th>prefix</th>
+    </tr>
 </html>
