5e039a81f1ba312110a85b590a7ea1dd398de47b
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-04 22:00:56 -0500

    Update course-section.php

--- course-section.php
+++ course-section.php
@@ -49,7 +49,7 @@ if ($result->num_rows > 0) {
     <td><?=$row["section_number"]?></td>
     <td><?=$row["instructor_name"]?>        <form method="post" action="instructors.php">
         <input type="hidden" name="id" value="<?=$row["CourseID"]?>" />
-        <input type="submit" value="Sections" />
+        <input type="submit" value="Click for more information on this professor" />
       </form> </td>
         </tr>
 <?php
