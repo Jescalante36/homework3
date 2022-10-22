eafc46193beb76147a6946793a4713f72ce16038
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-22 01:42:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from Course";
+$sql = "SELECT CourseID, prefix, number, description, m.CampusID from Course c join Campus m on m.CampusID = C.CampusID ";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
