18708b321062654826d199f6c25016bc10361162
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-21 23:07:09 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -32,7 +32,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from courses";
+$sql = "SELECT * from Course";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
