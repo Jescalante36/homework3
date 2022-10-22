ba7c2a915b5cc74563c27fa807b405830f2522a0
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-22 00:24:47 -0500

    Create Campus.php

---
+++ Campus.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Instructors</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Campus</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Campus ID</th>
+      <th>Campus Name</th>
+      <th>Course ID</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "jescalan_homework3";
+$password = "Angelito54321&";
+$dbname = "jescalan_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT CampusID, Campus_Name, c.CourseID from Campus m join Course c on c.CourseID = m.CourseID";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["CampusID"]?></td>
+    <td><?=$row["Campus_Name"]?></td>
+    <td><?=$row["CourseID"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
