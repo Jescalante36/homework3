cf9d8f76093c4dedc8952c9e08f2bf4347c3c8e3
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-22 01:12:34 -0500

    Update instructor-section.php

--- instructor-section.php
+++ instructor-section.php
@@ -31,7 +31,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$iid = $_GET['id'];
+$iid = $_POST['id'];
 //echo $iid;
 $sql = "select section_id, section_number, i.instructor_name, c.prefix, c.number from Sections s join instructor i on i.instructor_id = s.instructor_id join Course c on c.CourseID = s.CourseID where i.instructor_id=" . $iid;
 //echo $sql;
