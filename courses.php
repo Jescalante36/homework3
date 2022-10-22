194a34b880eb7131b6e7811b8e33bc2058ba4950
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-10-22 00:32:58 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -3,19 +3,16 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Courses</title>
+    <title>Instructors</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Courses</h1>
+    <h1>Instructors</h1>
 <table class="table table-striped">
   <thead>
     <tr>
       <th>ID</th>
-      <th>Prefix</th>
-      <th>Number</th>
-      <th>Description</th>
-      <th></th>
+      <th>Name</th>
     </tr>
   </thead>
   <tbody>
@@ -32,7 +29,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from Course";
+$sql = "SELECT instructor_id, instructor_name from instructor";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -40,16 +37,8 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
-    <td><?=$row["CourseID"]?></td>
-    <td><?=$row["prefix"]?></td>
-    <td><?=$row["number"]?></td>
-    <td><?=$row["description"]?></td>
-    <td>
-      <form method="post" action="course-section.php">
-        <input type="hidden" name="id" value="<?=$row["course_id"]?>" />
-        <input type="submit" value="Sections" />
-      </form>
-    </td>
+    <td><?=$row["instructor_id"]?></td>
+    <td><?=$row["instructor_name"]?></td>
   </tr>
 <?php
   }
