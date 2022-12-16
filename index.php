63dd6a91fd265ddd5e1438e86ffb696181372cbe
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-12-16 11:52:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <body>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
       <br>Instructor table that shows instructors with hyperlink filter</br>
-      <br>Courses table that shows courses taught.</br>
+      <br>Courses table that shows courses,number of course and in what campus is the course taught.</br>
       <br>Sections table that shows information about sections with post filter.</br>
       <br>Campus table that shows you campus class, no filter</br>
   </body>
