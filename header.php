1b8e6c526e1a69243d4006cfcf7c0468bb674597
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-12-15 18:36:43 -0600

    Update header.php

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="./course.php">Home</a>
+          <a class="nav-link" href="./course.php">Course</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="./instructors.php">Instructors</a>
