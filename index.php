9b9c13e1aaf3d478235ce1ca732a839a67798594
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-12-16 11:51:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 
   <body>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-      <br>Instructor - Table that shows instructors</br>
-      <br>Courses Table that shows courses taught.</br>
-      <br>Sections Table shows information about sections, no filter.</br>
-      <br>Campus - Table shows information about campus, allows you to filter by car color.</br>
+      <br>Instructor table that shows instructors with hyperlink filter</br>
+      <br>Courses table that shows courses taught.</br>
+      <br>Sections table that shows information about sections with post filter.</br>
+      <br>Campus table that shows you campus class, no filter</br>
   </body>
